b94562c624aa9b2f0d966eb764f4b85ec9bfe67e
dojo2022-shirakawa <shirakawa-misa-plusdojo2022@seplus2016.onmicrosoft.com>
2022-06-16 14:45:53 +0900

    HandmadeDiaryDAO

---
+++ src/src/dao/HandmadeDiaryDAO.java
@@ -0,0 +1,24 @@
+package dao;
+
+import java.util.ArrayList;
+
+import model.HandmadeDiaryBeans;
+
+public class HandmadeDiaryDAO {
+
+	public ArrayList<HandmadeDiaryBeans> selectLunch(String distance, String time, String category, String cost, String resName) {
+		return null;
+	}
+
+	public ArrayList<HandmadeDiaryBeans> select() {
+		return null;
+	}
+
+	public boolean updateLd(String lunchId, String emailAddress, String foodType,String resName, String foodPhoto, String category, String style, String date, String foodName, String cost, String time, String distance, int star) {
+		return false;
+	}
+
+	public boolean updateLdFlag(int  lunchFlag) {
+		return false;
+	}
+}
