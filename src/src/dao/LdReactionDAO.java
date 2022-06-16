024c5b9d23158979231da85637b717582e0fae54
dojo2022-yamashita <103239273+dojo2022-yamashita@users.noreply.github.com>
2022-06-16 13:54:27 +0900

    山下
    HdReactionDAOとHdReactionDAOのひながた作りました

---
+++ src/src/dao/LdReactionDAO.java
@@ -0,0 +1,29 @@
+package dao;
+
+import java.util.List;
+
+import model.LunchReactionBeans;
+
+public class LdReactionDAO {
+	public List<LunchReactionBeans> selectLdReaction(int ldReactionId, int lunchId, String emailAddress, int ldToGo, int ldToTell, int ldToUse) {
+		/*
+		Connection conn = null;
+		List<LunchReactionBeans> cardList = new ArrayList<LunchReactionBeans>();
+		*/
+		return null;
+	}
+	public boolean insertLdReaction(int ldReactionId, int lunchId, String emailAddress, int ldToGo, int ldToTell, int ldToUse) {
+		/*
+		Connection conn = null;
+		boolean result = false;
+		*/
+		return false;
+	}
+	public boolean deleteLdReaction(int ldReactionId) {
+		/*
+		Connection conn = null;
+		boolean result = false;
+		*/
+		return false;
+	}
+}
