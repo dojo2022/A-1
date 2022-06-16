7d279e99f58dbf81585df3764c70cc3138fd1fe1
dojo2022-akutagawa <103239164+dojo2022-akutagawa@users.noreply.github.com>
2022-06-16 14:49:25 +0900

    HdCommentDAO新規作成

---
+++ src/src/dao/HdCommentDAO.java
@@ -0,0 +1,17 @@
+package dao;
+
+import java.util.List;
+
+import model.HandmadeCommentBeans;
+
+public class HdCommentDAO {
+	public List<HandmadeCommentBeans> selectHdCommen(int hdCommentId, int handmadeId, String emailAddress, String hdComment) {
+		return null;
+	}
+	public boolean insertHdComment(int hdCommentId, int handmadeId, String emailAddress, String hdComment) {
+		return false;
+	}
+	public boolean deleteHdComment(int hdCommentId, int handmadeId, String emailAddress, String hdComment) {
+		return false;
+	}
+}
