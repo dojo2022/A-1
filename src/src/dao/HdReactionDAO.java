35e2f72db0a2f778082d5ec62614b52971831780
dojo2022-Moriya <103239214+dojo2022-Moriya@users.noreply.github.com>
2022-06-16 14:02:53 +0900

    HdReactionDAO、ListReactionDAOのメソッドひな形作成

---
+++ src/src/dao/HdReactionDAO.java
@@ -0,0 +1,20 @@
+package dao;
+
+import java.util.ArrayList;
+
+import model.HandmadeReactionBeans;
+
+public class HdReactionDAO {
+	//メソッドのひな形
+	public ArrayList<HandmadeReactionBeans> selectHdReaction(int hdReactionId, int handmadeId, String emailAddress, int hdToEat, int hdToTell, int hdToUse){
+		return null;
+	}
+
+	public boolean insertHdReaction(int hdReactionId, int handmadeId, String emailAddress, int hdToEat, int hdToTell, int hdToUse) {
+		return false;
+	}
+
+	public boolean deleteHdReaction(int hdReactionId) {
+		return false;
+	}
+}
---
+++ src/src/dao/ListReactionDAO.java
@@ -0,0 +1,19 @@
+package dao;
+
+import java.util.ArrayList;
+
+import model.ListReactionBeans;
+
+public class ListReactionDAO {
+	//メソッドのひな形
+	public ArrayList<ListReactionBeans> selectListReaction(int listReactionId, String emailAddress, int listToGo, int listToTell, int listToUse){
+		return null;
+	}
+	public boolean insertListReaction(int listReactionId, String emailAddress, int listToGo, int listToTell, int listToUse) {
+		return false;
+	}
+	public boolean deleteListReaction(int listReactionId, String emailAddress, int listToGo, int listToTell, int listToUse) {
+		return false;
+	}
+
+}
