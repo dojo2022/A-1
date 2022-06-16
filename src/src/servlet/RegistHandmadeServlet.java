54ce1239e8dec36a6a5830ac614a7e73af502a29
dojo2022-suyama <103238165+dojo2022-suyama@users.noreply.github.com>
2022-06-16 13:25:10 +0900

    各サーブレットのひな形作成
    EditHandmadeServletとRegistHandmadeServletのひな形です

--- src/src/servlet/RegistHandmadeServlet.java
+++ src/src/servlet/RegistHandmadeServlet.java
@@ -1,6 +1,8 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,8 +20,9 @@ public class RegistHandmadeServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//手作り記録登録画面ににフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/regist_diary.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
