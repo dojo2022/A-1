def86e5618675c0b8e3490fd118bb7b4a09b7d8d
dojo2022-yamashita <103239273+dojo2022-yamashita@users.noreply.github.com>
2022-06-22 13:19:59 +0900

    山下
    HdReactionDAO,ListReactionDAOを書きました
    が、動く保証はないです

--- src/src/dao/ListReactionDAO.java
+++ src/src/dao/ListReactionDAO.java
@@ -1,24 +1,116 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
-import model.ListReactionBeans;
+import model.LunchReactionBeans;
 
 public class ListReactionDAO {
 
-	//行きたい場所リストにスタンプを押した件数とユーザーを検索するメソッド
-	public ArrayList<ListReactionBeans> selectListReaction(int listReactionId, String emailAddress, int listToGo, int listToTell, int listToUse){
+		Connection conn = null;
+		ArrayList<LunchReactionBeans> listReaction = new 		ArrayList<LunchReactionBeans>();
+
+	//ランチ日記のスタンプの件数とユーザーの検索を行うメソッド
+	public ArrayList<LunchReactionBeans> selectListReaction(int listReactionId, int listId, String emailAddress, int listToGo, int listToTell, int listToUse) {
+
 		return null;
+
 	}
 
-	//行きたい場所リストのスタンプを登録するメソッド
-	public boolean insertListReaction(int listReactionId, String emailAddress, int listToGo, int listToTell, int listToUse) {
-		return false;
+	//ランチ日記のスタンプを登録するメソッド
+	public boolean insertListReaction(int listId, String emailAddress, int listToGo, int listToTell, int listToUse) {
+
+		boolean result = false;
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6Data/dojo6Data", "sa", "");
+
+			// SQL文を用意
+			String sql = "insert into list_reaction (list_id, email_address,list_to_go,list_to_tell,list_to_use) values (? ,? ,? ,? ,? )";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			// SQL文を完成させる
+			pStmt.setInt(1, listId);
+			pStmt.setString(2, emailAddress);
+			pStmt.setInt(3, listToGo);
+			pStmt.setInt(4, listToTell);
+			pStmt.setInt(5, listToUse);
+
+			// SQL文を実行する
+			if (pStmt.executeUpdate() == 1) {
+				result = true;
+			}
+		}
+			catch (SQLException e) {
+				e.printStackTrace();
+			}
+			catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+			finally {
+				// データベースを切断
+				if (conn != null) {
+					try {
+						conn.close();
+					}
+					catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+
+		return result;
+
 	}
 
-	//行きたい場所リストのスタンプを解除するメソッド
-	public boolean deleteListReaction(int listReactionId, String emailAddress, int listToGo, int listToTell, int listToUse) {
-		return false;
+	//ランチ日記のスタンプを削除するメソッド
+	public boolean deleteListReaction(int listReactionId) {
+		boolean result = false;
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6Data/dojo6Data", "sa", "");
+
+			// SQL文を用意
+			String sql = "delete from list_reaction where list_id = ?";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			// SQL文を完成させる
+			pStmt.setInt(1, listReactionId);
+
+			// SQL文を実行する
+			if (pStmt.executeUpdate() == 1) {
+				result = true;
+			}
+		}
+			catch (SQLException e) {
+				e.printStackTrace();
+			}
+			catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+			finally {
+				// データベースを切断
+				if (conn != null) {
+					try {
+						conn.close();
+					}
+					catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+
+		return result;
 	}
 
 }
