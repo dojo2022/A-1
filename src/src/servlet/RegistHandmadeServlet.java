a894de2e29eb82817a8e2504ec99b237548f360b
dojo2022-shirakawa <shirakawa-misa-plusdojo2022@seplus2016.onmicrosoft.com>
2022-06-22 11:50:48 +0900

    Update RegistHandmadeServlet.java

--- src/src/servlet/RegistHandmadeServlet.java
+++ src/src/servlet/RegistHandmadeServlet.java
@@ -9,7 +9,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.servlet.http.Part;
+
+import dao.HandmadeDiaryDAO;
+import model.UserMasterBeans;
 
 
 /**
@@ -19,13 +23,14 @@ import javax.servlet.http.Part;
 @WebServlet("/RegistHandmadeServlet")
 public class RegistHandmadeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private HttpServletRequest request;
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//手作り記録登録画面ににフォワードする
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/regist_diary.jsp");
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/regist_handmade_diary.jsp");
 		dispatcher.forward(request, response);
 	}
 
@@ -33,7 +38,12 @@ public class RegistHandmadeServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		 doGet(request, response);
+		doGet(request, response);
+
+		//insertに必要なメールアドレスを取得
+		HttpSession session = request.getSession();
+		UserMasterBeans user = (UserMasterBeans) session.getAttribute("user");
+		String emailAddress = user.getEmailAddress();
 
 		 // リクエストパラメータを取得する
 		 request.setCharacterEncoding("UTF-8");
@@ -42,45 +52,50 @@ public class RegistHandmadeServlet extends HttpServlet {
 		 String cookTime = request.getParameter("cookTime");
 		 String date = request.getParameter("date");
 		 String cost = request.getParameter("cost");
-		 String star = request.getParameter("star");
+		 int star = Integer.parseInt(request.getParameter("star"));
 		 String feeling = request.getParameter("feeling");
-	}
-}
 
 
-	/*	String image = this.getFileName(foodPhoto);
+		 String image = this.getFileName(foodPhoto);
 
-		HttpSession session = request.getSession();
-		request.setAttribute("image", image);
-		// サーバの指定のファイルパスへファイルを保存
-        //場所はクラス名↑の上に指定してある
+		 request.setAttribute("image", image);
+		//  サーバの指定のファイルパスへファイルを保存
+		//場所はクラス名↑の上に指定してある
 
 
 		//	DAOを呼び出す
 		HandmadeDiaryDAO hDao = new HandmadeDiaryDAO();
+		boolean ans = hDao.insertHd(emailAddress, foodName,image,cookTime,date,cost,star,feeling);
 
-		//登録処理を行う
-		if(hDao.insert(foodName,foodPhoto,cookTime,date,cost,star,feeling)) {
-			request.setAttribute("result","手作り日記をを登録しました。");
-		}else {												// 登録失敗
-			request.setAttribute("result","登録できませんでした");
+		// 登録処理
+		// 成功した時
+		if(ans == true) {
+			request.setAttribute("hdresult","手作り日記を登録しました。");
+		// 失敗した時
+		}else {
+			request.setAttribute("hdresult","登録できませんでした");
 		}
 		//タイムラインに遷移
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/timeline.jsp");
 		dispatcher.forward(request, response);
+		return;
 	}
 
+
 		//ファイルの名前を取得してくる
-			private String getFileName(Part part) {
+			private String getFileName(Part foodPhoto) {
 			    String name = null;
-			    for (String dispotion : part.getHeader("Content-Disposition").split(";")) {
+			    for (String dispotion : foodPhoto.getHeader("Content-Disposition").split(";")) {
 			        if (dispotion.trim().startsWith("filename")) {
 			            name = dispotion.substring(dispotion.indexOf("=") + 1).replace("\"", "").trim();
 			            name = name.substring(name.lastIndexOf("\\") + 1);
 			            break;
 			        }
-			    }		// TODO 自動生成されたメソッド・スタブ
+			    }
 				return name;
 			}
-			*/
+
+		}
+
+
 
